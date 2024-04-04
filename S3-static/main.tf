f300119b40af5e76fc35923f23ec318d794269a8
Anncodes001 <annalexx2001@gmail.com>
2024-04-04 19:05:30 +0100

    tf

--- S3-static/main.tf
+++ S3-static/main.tf
@@ -61,27 +61,26 @@ module "template_files"{
 
 resource "aws_s3_bucket_website_configuration" "web-config" {
   bucket = aws_s3_bucket.resume-bucket.id
-
-
   index_document {
     suffix = "index.html"
   }
+}
 
- 
+
 resource "aws_s3_bucket_object" "static_files" {
-  for_each = module.template_files.files
-
-  bucket       = aws_s3_bucket.resume-bucket.id
-  key          = each.key
-  content_type = each.value.content_type
+  bucket        = aws_s3_bucket.resume-bucket.id
+  
+  for_each      = module.template_files.files
+  key           = each.key
+  content_type  = each.value.content_type
 
   # The template_files module guarantees that only one of these two attributes
   # will be set for each file, depending on whether it is an in-memory template
   # rendering result or a static file on disk.
-  source  = each.value.source_path
-  content = each.value.content
+  source        = each.value.source_path
+  content       = each.value.content
 
   # Unless the bucket has encryption enabled, the ETag of each object is an
   # MD5 hash of that object.
-  etag = each.value.digests.md5
+  etag          = each.value.digests.md5
 }
