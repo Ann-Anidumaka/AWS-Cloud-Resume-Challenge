74577f7a2bfec05d16b2b41e8585587ab3840eb3
Anncodes001 <annalexx2001@gmail.com>
2024-04-19 13:31:41 +0100

    lambda corrected to correctly reflect the +1 increaments

--- lambda_function.py
+++ lambda_function.py
@@ -1,33 +1,14 @@
+import json
 import boto3
-#from boto3.dynamodb.conditions import Key
-import os
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('cloud_resume')
 
 def lambda_handler(event, context):
+    response = table.get_item(Key={'id': '1'})
+    views = response['Item']['views']
+    views = views + 1
+    print(views)
+    response = table.put_item(Item={'id': '1', 'views': views})
     
-    TABLE_NAME = "cloud_resume"
-    db_client = boto3.client('dynamodb')
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(TABLE_NAME)
-
-    update = db_client.update_item(
-        TableName=TABLE_NAME,
-        Key={"id": {"N": "0"}},
-        UpdateExpression="ADD views :inc",
-        ExpressionAttributeValues={":inc": {"N": "1"}}
-    )
-
-    getItems = table.get_item(Key={"id": 0})
-    itemsObjectOnly = getItems["Item"]
-    visitcount = itemsObjectOnly["views"]
-
-    response = {
-        "headers": {
-            "content-type" : "application/json"
-        },
-        "status_code": 200,
-        "body" : {
-            "count": views
-        }
-    }
-
-    return response
+    return views
